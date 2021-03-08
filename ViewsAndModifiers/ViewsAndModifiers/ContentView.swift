58ed8aa948f0189dd91bf90a37b890263b65450b
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2021-03-08 21:03:05 +0100

    feat: day 23 — custom view and modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Alexis Beaudoin on 08/03/2021.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    var name: String
+    
+    var body: some View {
+        Text("People call me \(name)!")
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack (alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .padding(5)
+                .background(Color.black)
+                .foregroundColor(Color.white)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {
+    @State private var lineInRed = false
+    
+    private var line1 = Text("Hello, world!")
+    private var line2: some View { Text("Hello, Twitter!") }
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.lineInRed.toggle()
+            }) {
+                Text("Change color")
+                    .foregroundColor(self.lineInRed ? .blue : .red)
+            }
+            line1
+                .foregroundColor(self.lineInRed ? .red : .blue)
+            line2
+                .foregroundColor(self.lineInRed ? .red : .blue)
+            CustomText(name: "Rym")
+            Color.gray
+                .frame(width: 200, height: 200)
+                .watermarked(with: "Rymfire")
+        }.blur(radius: 1)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
