1a7ed5455a1f6b5633e69b66c4ea57f1b0c73834
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2021-09-20 12:35:24 +0200

    other: day 26 — betterrest project created

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexis Beaudoin on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
