8288cc63f38ebe04db7a6cdeaf56bb0c05ddea8a
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2021-10-19 12:46:00 +0200

    other: d29: learned to load ressources from app bundle

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,73 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var wifiEnabled = false
-    
-    private let networks = ["We have an Hulk", "IONIS", "Livebox-2a88", "SFR-1a43", "SFR-0909", "Xiaomi phone", "Galaxy S21 de Maximilien"]
-    
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    Toggle("Wi-FI", isOn: $wifiEnabled)
-                }
-                
-                if wifiEnabled {
-                    Section(header: Text("Networks")) {
-                        ForEach (networks, id: \.self) { network in
-                            HStack{
-                                Text(network)
-                                Spacer()
-                                HStack {
-                                    Image(systemName: "lock.fill")
-                                    Image(systemName: "wifi")
-                                    Image(systemName: "info.circle").foregroundColor(.blue)
-                                }
-                            }
-                        }
-                        Text("Other...")
-                    }
-                    
-                    Section {
-                        HStack {
-                            Text("Ask to join Networks")
-                            Spacer()
-                            HStack {
-                                Text("Ask")
-                                    .foregroundColor(.gray)
-                                Image(systemName: "chevron.right")
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    }
-                    
-                    
-                    Section {
-                        HStack {
-                            Text("Auto-join Hotspot")
-                            Spacer()
-                            HStack {
-                                Text("Ask to Join")
-                                    .foregroundColor(.gray)
-                                Image(systemName: "chevron.right")
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    }
-                }
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Wi-Fi").font(.headline)
-                }
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            // we found the file in our bundle
+            if let fileContents = try? String(contentsOf: fileURL) {
+                // try? automatically catches the thrown error and makes the return nil.
+                // loaded the file in string.
             }
         }
+        Text("Hello world").padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(wifiEnabled: true)
+        ContentView()
     }
 }
