70e413d31f4533c141b5b0bf0a729e4234a9fb09
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2021-10-19 14:36:34 +0200

    feat-wip: d29-30 – WordScramble app started
    + random root word from start.txt
    + users can enter words using text input
    + input history

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,15 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rootWord = ""
+    @State private var usedWords = [String]()
+    @State private var newWord = ""
+    
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            // we found the file in our bundle
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // try? automatically catches the thrown error and makes the return nil.
-                // loaded the file in string.
+        NavigationView {
+            VStack {
+                TextField("Enter new word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding(.horizontal, 16)
+                List (usedWords, id: \.self) { word in // FIXME: Grey background
+                    HStack(alignment: .center) {
+                        Image.init(systemName: "\(word.count).circle")
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onAppear(perform: resetRootWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        //TODO: extra validation to come
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func resetRootWord() { // Reset game session
+        if let fileURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // File was found
+            if let fileContent = try? String(contentsOf: fileURL) {
+                // File contains text
+                let allRootWords = fileContent.components(separatedBy: "\n")
+                self.rootWord = allRootWords.randomElement() ?? "silkworm"
+                //TODO: Empty usedWords
+                return
             }
         }
-        Text("Hello world").padding()
+        fatalError("Could not load start.txt from bundle.")
     }
 }
 
