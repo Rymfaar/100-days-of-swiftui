a0e75ee74eed0a3bd932566b2d28840584550fef
Alexis Beaudoin <alexis.beaudoin06@gmail.com>
2021-11-02 13:26:29 +0100

    feat: d31 — wordscramble completed

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    @State private var score = 0
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -25,20 +31,56 @@ struct ContentView: View {
                         Text(word)
                     }
                 }
+                Spacer()
             }
             .navigationTitle(rootWord)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: resetRootWord) {
+                        Text("Restart")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("\(self.score) points")
+                }
+            }
             .onAppear(perform: resetRootWord)
         }
+        .alert(isPresented: $showingError) {
+            Alert(title: Text(self.errorTitle), message: Text(self.errorMessage), dismissButton: .default(Text("OK")))
+        }
     }
-    
+
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        guard answer.count > 0 else {
+        guard answer.count >= 3 else {
+            wordError(title: "Word \"\(answer)\" is too short", message: "Way too easy dude")
+            self.newWord = ""
             return
         }
-        //TODO: extra validation to come
-        usedWords.insert(answer, at: 0)
+        guard answer.lowercased() != self.rootWord.lowercased() else {
+            wordError(title: "This is the exact same word", message: "Come on...")
+            self.newWord = ""
+            return
+        }
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word \"\(answer)\" used already", message: "Be more original")
+            self.newWord = ""
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "Word \"\(answer)\" not recognized", message: "You can't juste make them up, you know !")
+            self.newWord = ""
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "Word \"\(answer)\" not possible", message: "That isn't a real word")
+            self.newWord = ""
+            return
+        }
+        self.usedWords.insert(answer, at: 0)
+        self.score += 10 * answer.count
         newWord = ""
     }
     
@@ -49,11 +91,44 @@ struct ContentView: View {
                 // File contains text
                 let allRootWords = fileContent.components(separatedBy: "\n")
                 self.rootWord = allRootWords.randomElement() ?? "silkworm"
-                //TODO: Empty usedWords
+                // Empty used words lists and text field (in case of restart)
+                self.usedWords.removeAll()
+                self.newWord = ""
                 return
             }
         }
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord.lowercased()
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool { //!!!: Check if word is real
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        self.errorTitle = title
+        self.errorMessage = message
+        self.showingError.toggle()
     }
 }
 
